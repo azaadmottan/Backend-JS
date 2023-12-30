917c6f27f8c4e9bf921e745fc42edf196bb01666
Azaad Mottan <azaadmottan2022@gmail.com>
2023-12-30 19:01:56 +0530

    Add router for user-register and also configure 'postman' for api request.

---
+++ Backend/src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controllers.js";
+
+
+const userRouter = Router();
+
+userRouter.route("/register").post(registerUser);
+
+
+export default userRouter;
