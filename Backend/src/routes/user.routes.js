092d7df8eb3d43277c08f6396bbbba5e0fe86ec2
Azaad Mottan <azaadmottan2022@gmail.com>
2023-12-31 16:28:17 +0530

    Add login and logout router.

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,22 +1,28 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.route("/register").post(
-        upload.fields([
-            {
-                name: "avatar",
-                maxCount: 1
-            },
-            {
-                name: "coverImage",
-                maxCount: 1
-            }
-        ]),
-        registerUser
-    );
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+);
+
+userRouter.route("/login").post(loginUser);
+
+// secure route
+userRouter.route("/logout").post(verifyJWT, logoutUser);
 
 
 export default userRouter;
