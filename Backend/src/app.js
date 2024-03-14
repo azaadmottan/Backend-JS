f2004b14911724cfefc6e72531218bd28112266e
Azaad Mottan <azaadmottan2022@gmail.com>
2024-03-14 17:13:42 +0530

    Add video router and some modification & increase data limit.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,12 +11,12 @@ app.use(cors({
 }));
 
 app.use(express.json({
-    limit: "16kb"
+    limit: "50mb"
 }));
 
 app.use(express.urlencoded({
     extended: true,
-    limit: "16kb"
+    limit: "50mb"
 }));
 
 app.use(express.static("public"));
@@ -26,9 +26,11 @@ app.use(cookieParser());
 
 // import routes
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);      
+app.use("/api/v1/videos", videoRouter);
 
 // the route will become
 
