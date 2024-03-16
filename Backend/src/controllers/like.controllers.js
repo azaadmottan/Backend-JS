ce3e60ba1826ddb0a8827b04ef3d2211cdf2cd88
Azaad Mottan <azaadmottan2022@gmail.com>
2024-03-16 15:02:41 +0530

    Create controller method i.e getLikedVideos

--- Backend/src/controllers/like.controllers.js
+++ Backend/src/controllers/like.controllers.js
@@ -39,8 +39,76 @@ const toggleLikeVideo = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, { isLiked: true }, "Add like"));
 });
 
+// get user liked videos 
+
+const getLikedVideos = asyncHandler(async (req, res) => {
+
+    const likedVideoAggregate = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id)
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "ownerDetails",
+                        }
+                    }, 
+                    {
+                        $unwind: "$ownerDetails",
+                    }
+                ]
+            },
+        }, 
+        {
+            $unwind: "$likedVideo",
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        }, 
+        {
+            $project: {
+                _id: 0,
+                likedVideo: {
+                    _id: 1,
+                    title: 1,
+                    description: 1,
+                    "videoFile.url": 1,
+                    "thumbnail.url": 1,
+                    duration: 1,
+                    views: 1,
+                    createdAt: 1,
+                    isPublished: 1,
+                    owner: 1,
+                    ownerDetails: {
+                        userName: 1,
+                        fullName: 1,
+                        "avatar": 1,
+                    }
+                }
+            }
+        }
+    ]);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, likedVideoAggregate, "Liked videos fetched successfully"));
+});
 
 
 export {
     toggleLikeVideo,
+    getLikedVideos,
 }
--- Backend/src/routes/like.routes.js
+++ Backend/src/routes/like.routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { 
+    getLikedVideos,
     toggleLikeVideo,
 } from '../controllers/like.controllers.js';
 
@@ -11,6 +12,6 @@ router.use(verifyJWT);  // Apply verifyJWT middleware to all routes in this file
 // route for like video
 
 router.route("/toggleLike/v/:videoId").post(toggleLikeVideo);
-
+router.route("/getLikedVideos").get(getLikedVideos);
 
 export default router;
